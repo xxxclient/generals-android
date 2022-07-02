c2310e28881472de47f5518b05d030f515500560
Pavel Potapov <potapov@goozix.com>
2022-07-02 03:19:04 +0300

    Added dialogs

--- app/src/main/java/com/potapov/generals/presentation/view/GameFragment.kt
+++ app/src/main/java/com/potapov/generals/presentation/view/GameFragment.kt
@@ -1,12 +1,16 @@
 package com.potapov.generals.presentation.view
 
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.potapov.generals.databinding.DialogBattleBinding
 import com.potapov.generals.databinding.FragmentGameBinding
 import com.potapov.generals.domain.entity.Statistic
 
@@ -17,6 +21,10 @@ class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    private var _bindingDialogBattle: DialogBattleBinding? = null
+    private val bindingDialogBattle: DialogBattleBinding
+        get() = _bindingDialogBattle ?: throw RuntimeException("DialogBattleBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,24 +37,30 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnBattleDialog.setOnClickListener { showBattleDialog(true) }
-        binding.dialogBattle.btnToBattle.setOnClickListener {
-            launchBattleFragment(Statistic.DEFAULT)
+        binding.btnBattleDialog.setOnClickListener {
+            _bindingDialogBattle = DialogBattleBinding.inflate(layoutInflater)
+            showBattleDialog()
         }
     }
 
-    private fun launchBattleFragment(statistic: Statistic) {
-        findNavController().navigate(
-            GameFragmentDirections.actionGameFragmentToBattleFragment(statistic)
-        )
+    private fun showBattleDialog() = Dialog(requireContext()).apply {
+        window?.setBackgroundDrawable(ColorDrawable(android.R.color.transparent))
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(bindingDialogBattle.root)
+        bindingDialogBattle.btnToBattle.setOnClickListener {
+            dismiss()
+            launchBattleFragment(Statistic.DEFAULT)
+        }
+        show()
     }
 
-    private fun showBattleDialog(show: Boolean) {
-        binding.dialogBattle.root.visibility = if (show) View.VISIBLE else View.GONE
-    }
+    private fun launchBattleFragment(statistic: Statistic) = findNavController().navigate(
+        GameFragmentDirections.actionGameFragmentToBattleFragment(statistic)
+    )
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _bindingDialogBattle = null
         _binding = null
     }
 }
