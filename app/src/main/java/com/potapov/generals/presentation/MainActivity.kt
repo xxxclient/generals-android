7180f4bd67613b440d8581bc55a7682956e02d48
Pavel Potapov <potapov@goozix.com>
2022-07-15 22:41:10 +0300

    Added media player

--- app/src/main/java/com/potapov/generals/presentation/MainActivity.kt
+++ app/src/main/java/com/potapov/generals/presentation/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.potapov.generals.presentation
 
+import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.potapov.generals.R
 import com.potapov.generals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,16 +17,28 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding
         get() = _binding ?: throw RuntimeException("ActivityMainBinding == null")
 
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         hideSystemBars()
+
+        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.bg_3)
+        mediaPlayer.isLooping = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.pause()
     }
 
     override fun onResume() {
         super.onResume()
         hideSystemBars()
+        // TODO: Включить музыку!
+//        mediaPlayer.start()
     }
 
     private fun hideSystemBars() {
